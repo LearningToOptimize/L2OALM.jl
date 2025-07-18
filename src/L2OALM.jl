07324830e0a8754fcc15d15036f527b734c18269
Andrew <arosemberg3@gatech.edu>
2025-07-18 16:16:55 -0400

    add primal dual losses

--- src/L2OALM.jl
+++ src/L2OALM.jl
@@ -1,5 +1,78 @@
 module L2OALM
 
-# Write your package code here.
+using BatchNLPKernels
+using ExaModels
+using KernelAbstractions
+using DifferentiationInterface
+const DI = DifferentiationInterface
+
+import Zygote
+import FiniteDifferences
+
+using PowerModels
+PowerModels.silence()
+using PGLib
+using LinearAlgebra
+
+using Lux
+using LuxCUDA
+using Lux.Training
+using MLUtils
+using Optimisers
+using CUDA
+using Random
+import GPUArraysCore: @allowscalar
+
+using OpenCL, pocl_jll, AcceleratedKernels
+
+function LagrangianDualLoss(;max_dual=1e6)
+    return (dual_model, ps_dual, st_dual, data) -> begin
+        x, dual_hat_k, gh, ρ = data
+        
+        # Get current dual predictions
+        dual_hat, st_dual_new = dual_model(x, ps_dual, st_dual)
+        
+        # Separate bound and equality constraints
+        gh_bound = gh[1:end-n_bus*2,:]
+        gh_equal = gh[end-n_bus*2+1:end,:]
+        dual_hat_bound = dual_hat_k[1:end-n_bus*2,:]
+        dual_hat_equal = dual_hat_k[end-n_bus*2+1:end,:]
+        
+        # Target for dual variables
+        dual_target = vcat(
+            min.(max.(dual_hat_bound + ρ .* gh_bound, 0), max_dual),
+            min.(dual_hat_equal + ρ .* gh_equal, max_dual)
+        )
+        
+        loss = mean((dual_hat .- dual_target).^2)
+        return loss, st_dual_new, (dual_loss=loss,)
+    end
+end
+
+function LagrangianPrimalLoss(bm::BatchModel)    
+    return (model, ps, st, data) -> begin
+        Θ, dual_hat, ρ = data
+        num_s = size(Θ, 2)
+
+        # Forward pass for prediction
+        X̂, st_new = model(Θ, ps, st)
+        
+        # Calculate violations and objectives
+        objs = BNK.objective!(bm, X̂, Θ)
+        # gh = constraints!(bm, X̂, Θ)
+        Vc, Vb = BNK.all_violations!(bm, X̂, Θ)
+        V = vcat(Vb, Vc)
+        total_loss = (
+            sum(abs.(dual_hat .* V)) / num_s +
+            ρ / 2 * sum((V).^2) / num_s +
+            mean(objs)
+        )
+
+        return total_loss, st_new, (
+            total_loss=total_loss,
+        )
+    end
+end
+
 
 end
